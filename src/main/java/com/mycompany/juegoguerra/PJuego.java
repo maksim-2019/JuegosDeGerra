8a02611512b93274447e036d8000aea021321300
Maksim <maksimsereda2@gmail.com>
2020-01-27 13:51:20 +0100

    c2

--- src/main/java/com/mycompany/juegoguerra/PJuego.java
+++ src/main/java/com/mycompany/juegoguerra/PJuego.java
@@ -41,45 +41,43 @@ public class PJuego {
         for (int i = 0; i < arraySoldado.length; i++) {
             //Permite poner nombre a cada participante.
 
-            System.out.println("Pon el nombre del jugador: " + i);
+            System.out.println("Pon el nombre del jugador " + i + ": ");
             nombre = teclado.next();
 
             jugadorArray.add(nombre + i);
             arraySoldado[i] = new Soldado(nombre + i, 100);
             arrayMunicion[i] = new Municion("9mm-" + i, 3);
             arrayArma[i] = new Arma("Pistola" + i, 30, arrayMunicion[i]);
-            System.out.println(arrayArma[i]);
             System.out.println(arraySoldado[i]);
             nombre = teclado.nextLine();
         }
         do {
             do {
-            do {
-                si1 = false;
-                uno = aleatorio.nextInt(participantes);
-                if (arraySoldado[uno].isMuerto() == false) {
-                    si1 = true;
-                }
-            } while (si1 != true);
-            do {
+                do {
+                    si1 = false;
+                    uno = aleatorio.nextInt(participantes);
+                    if (arraySoldado[uno].isMuerto() == false) {
+                        si1 = true;
+                    }
+                } while (si1 != true);
+                do {
                     si2 = false;
                     dos = aleatorio.nextInt(participantes);
-                if (arraySoldado[dos].isMuerto() == true) {
-                    dos = aleatorio.nextInt(participantes);
-                } else{
-                    si2 = true;
-                }
-            } while (si2 != true);
+                    if (arraySoldado[dos].isMuerto() == true) {
+                        dos = aleatorio.nextInt(participantes);
+                    } else {
+                        si2 = true;
+                    }
+                } while (si2 != true);
 
-             } while (uno == dos);
-              arraySoldado[uno].atacar(arrayArma[uno], arraySoldado[dos]);
+            } while (uno == dos);
+            arraySoldado[uno].atacar(arrayArma[uno], arraySoldado[dos]);
             if (arraySoldado[dos].isMuerto() == true) {
                 jugadorArray.remove(arraySoldado[dos].getNombre());
             }
             System.out.println("El " + arraySoldado[uno].getNombre() + " a atacado a " + arraySoldado[dos].getNombre());
             System.out.println(" Vida restante: " + arraySoldado[dos].getVida());
         } while (jugadorArray.size() != 1);
-        Iterator<String> jugadorIterator = jugadorArray.iterator();
-        System.out.println("El ganador es: " + jugadorIterator.next());
+        System.out.println("El ganador es: " + jugadorArray.get(0));
     }
 }
